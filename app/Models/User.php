4d0997db42665dc56ec6d9d9c7f5df4798bd9977
gracenan-500 <gracenantcop@gmail.com>
2026-05-18 13:33:54 +0100

    mode user version 1

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['firstname', 'email', 'password', 'lastname', 'phone', 'role', 'avatar'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
