687f4e1a0e8dd582a428b24ed09030508f1aaf62
RaoulKM <raoulkm2006@gmail.com>
2026-05-18 11:34:54 +0100

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->enum('role', ['user', 'admin', 'fournisseur'])->default('user');
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('avatar')->nullable();
             $table->string('password');
+
+            $table->softDeletes();
+
             $table->rememberToken();
             $table->timestamps();
         });
