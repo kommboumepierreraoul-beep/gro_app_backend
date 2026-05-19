d8d55d247f583762bb26cce415bb4bc4e05489b0
RaoulKM <raoulkm2006@gmail.com>
2026-05-19 14:58:21 +0100

    Update user.php

--- routes/users/user.php
+++ routes/users/user.php
@@ -3,15 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+Route::middleware(['auth:sanctum', 'verified.email'])->prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index']); // Get all users
+    Route::get('/{id}', [UserController::class, 'show']); // Get user by ID
+    Route::post('/', [UserController::class, 'store']); // Create new user
+    Route::put('/{id}', [UserController::class, 'update']); // Update user by ID
+    Route::delete('/{id}', [UserController::class, 'destroy']); // Delete user by ID
+    Route::post('/restore/{id}', [UserController::class, 'restore']); // Restore deleted user by ID
 
-
-
-//User routes
-Route::prefix('users', function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::get('/{id}', [UserController::class, 'show']);
-    Route::put('/{id}', [UserController::class, 'update']);
-    Route::delete('/{id}', [UserController::class, 'destroy']);
-
-    Route::post('/profile', [UserController::class, 'updateProfile'])->middleware('auth:sanctum');
 });
