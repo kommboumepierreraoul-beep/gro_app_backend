d23c25b80408cadc292d732a8c21b9980ecf7fa4
RaoulKM <raoulkm2006@gmail.com>
2026-05-26 18:59:43 +0100

    Create Message.php

---
+++ app/Models/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'conversation_id',
+        'sender_id',
+        'content',
+        'media_url',
+        'status',
+    ];
+
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id')
+            ->withDefault(['firstname' => 'Utilisateur supprimé']);
+    }
+
+    public function conversation(): BelongsTo
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+}
