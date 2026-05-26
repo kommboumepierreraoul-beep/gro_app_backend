d371acd163879658a60a106b80444d4f728b2785
RaoulKM <raoulkm2006@gmail.com>
2026-05-26 18:59:17 +0100

    Create Comment.php

---
+++ app/Models/Comment.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany, MorphMany};
+
+class Comment extends Model
+{
+    use SoftDeletes;
+    protected $fillable = ['post_id', 'user_id', 'parent_id', 'content', 'likes_count'];
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id')
+                    ->withDefault(['firstname' => 'Utilisateur supprimé']);
+    }
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+    public function replies(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'parent_id')->latest();
+    }
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
+    }
+    public function likes(): MorphMany
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+    public function isLikedBy(int $userId): bool
+    {
+        return $this->likes()->where('user_id', $userId)->exists();
+    }
+}
