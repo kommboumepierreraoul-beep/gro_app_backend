e9e89d4101c4cffc86c37a7208263851cb435445
ZANGUE PAUL <paolozangue1@example.com>
2026-06-29 20:49:42 +0100

    Correction DisputeResolvedMail et User

--- app/Mail/DisputeResolvedMail.php
+++ app/Mail/DisputeResolvedMail.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 namespace App\Mail;
 use App\Models\Dispute;
 use Illuminate\Bus\Queueable;
@@ -18,58 +17,5 @@ class DisputeResolvedMail extends Mailable
     public function content(): Content
     {
         return new Content(view: 'emails.disputes.resolved');
-=======
-
-namespace App\Mail;
-
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Attachment;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
-
-class DisputeResolvedMail extends Mailable
-{
-    use Queueable, SerializesModels;
-
-    /**
-     * Create a new message instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Dispute Resolved Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
->>>>>>> 9a5ebbd473ed8da6d7209a514372a33f63ea9ac2
     }
 }
